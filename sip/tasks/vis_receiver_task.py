192a0284971503d4d5981235ba8e991b96ee3bcc
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2017-10-04 16:52:01 +0100

    Temp change in vis_receiver task

--- sip/tasks/vis_receiver_task.py
+++ sip/tasks/vis_receiver_task.py
@@ -1,41 +1,81 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""Visibility receiver task module.
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+# """Visibility receiver task module.
+#
+# Implements C.1.2.1.4 from the product tree.
+# """
+#
+# import os
+# import sys
+#
+# import signal
+# import simplejson as json
+#
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
+#
+# from sip.processor_software.vis_receiver import VisReceiver
+# from sip.common.logging_api import log
+#
+#
+# def _sig_handler(signum, frame):
+#     sys.exit(0)
+#
+#
+# def main():
+#     """Task run method."""
+#     # Install handler to respond to SIGTERM
+#     signal.signal(signal.SIGTERM, _sig_handler)
+#
+#     # FIXME(FD) Get configuration data - it should not happen like this.
+#     with open(sys.argv[1]) as f:
+#         config = json.load(f)
+#
+#     # Create streams and receive SPEAD data.
+#     os.chdir(os.path.expanduser('~'))
+#     receiver = VisReceiver(config, log)
+#     receiver.run()
+#
+#
+# if __name__ == '__main__':
+#     main()
+#
 
-Implements C.1.2.1.4 from the product tree.
+
+# !/usr/bin/python3
+
+
+""" Skeleton process to be started by slave
+
 """
-
 import os
+import socket
 import sys
 
-import signal
-import simplejson as json
-
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
-
-from sip.processor_software.vis_receiver import VisReceiver
-from sip.common.logging_api import log
 
 
 def _sig_handler(signum, frame):
     sys.exit(0)
 
 
-def main():
-    """Task run method."""
-    # Install handler to respond to SIGTERM
-    signal.signal(signal.SIGTERM, _sig_handler)
+def run():
+    # Write to the host file system
+    f = open('/mnt/tmp/hello_nijin', 'w')
+    f.close()
 
-    # FIXME(FD) Get configuration data - it should not happen like this.
-    with open(sys.argv[1]) as f:
-        config = json.load(f)
+    # Read port number
+    port = int(sys.argv[1])
 
-    # Create streams and receive SPEAD data.
-    os.chdir(os.path.expanduser('~'))
-    receiver = VisReceiver(config, log)
-    receiver.run()
+    # Bind to socket
+    s = socket.socket()
+    s.bind(('', port))
+    s.listen(1)
+
+    conn, addr = s.accept()
+    while (True):
+        data = conn.recv(1024)
+    conn.close()
 
 
 if __name__ == '__main__':
-    main()
-
+    run()
